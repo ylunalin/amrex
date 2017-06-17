c0dfe37e2377377fdd37774180afcd12de30ad3f
Xinsheng (Shawn) Qin <xinshengqin2014@gmail.com>
2017-06-16 22:53:32 -0700

    add cudaDeviceReset() to amrex::Finalize()

--- Src/Base/AMReX.cpp
+++ Src/Base/AMReX.cpp
@@ -493,6 +493,14 @@ amrex::Finalize (bool finalize_parallel)
 #ifndef GASNET_CONDUIT_MPI
         ParallelDescriptor::EndParallel();
 #endif
+    // cudaDeviceReset causes the driver to clean up all state. While
+    // not mandatory in normal operation, it is good practice.  It is also
+    // needed to ensure correct operation when the application is being
+    // profiled. Calling cudaDeviceReset causes all profile data to be
+    // flushed before the application exits
+    cudaDeviceReset();
+#endif
+
     }
 }
 
