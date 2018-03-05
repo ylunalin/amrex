9cad267f97411717b3dfab06a2a484a70afb4732
Travis CI <atmyers@lbl.gov>
2018-03-05 17:37:24 +0000

    Deploy to GitHub Pages: 3efbb0e76318b75a1ec3223ba46d682145573d2f

--- doxygen/search/all_0.js
+++ doxygen/search/all_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['_5febfluxfab_5fh_5f',['_EBFLUXFAB_H_',['../_a_m_re_x___e_b_flux_f_a_b_8_h.html#a76b32f517f0d5db263475754041d4e0d',1,'AMReX_EBFluxFAB.H']]],
   ['_5ffluxreg_5ff_5fh_5f',['_FLUXREG_F_H_',['../_a_m_re_x___f_l_u_x_r_e_g___f_8_h.html#af7408d6e8c99e0d6f4978dd8d8c2701b',1,'AMReX_FLUXREG_F.H']]],
   ['_5fkdnode',['_kdnode',['../structamrex_1_1__kdnode.html',1,'amrex']]],
   ['_5fkdres',['_kdres',['../structamrex_1_1__kdres.html',1,'amrex']]],
@@ -9,6 +10,5 @@ var searchData=
   ['_5fpd_5fget_5fbit',['_pd_get_bit',['../namespaceamrex.html#ae59d387db41e0a212cc803bf4c72cf3b',1,'amrex']]],
   ['_5fpd_5finsert_5ffield',['_pd_insert_field',['../namespaceamrex.html#a8cea431d3f24bfcb6da0dae43ee0acb1',1,'amrex']]],
   ['_5fpd_5fset_5fbit',['_pd_set_bit',['../namespaceamrex.html#ac3daae71af784ea861908589aa58b9e9',1,'amrex']]],
-  ['_5fresultnode',['_resultnode',['../structamrex_1_1__resultnode.html',1,'amrex']]],
-  ['_5fslabservice_5fh_5f',['_SLABSERVICE_H_',['../_a_m_re_x___slab_service_8_h.html#ae121be40849f3a0a903f018721a7c16a',1,'AMReX_SlabService.H']]]
+  ['_5fresultnode',['_resultnode',['../structamrex_1_1__resultnode.html',1,'amrex']]]
 ];
--- doxygen/search/defines_0.js
+++ doxygen/search/defines_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['_5ffluxreg_5ff_5fh_5f',['_FLUXREG_F_H_',['../_a_m_re_x___f_l_u_x_r_e_g___f_8_h.html#af7408d6e8c99e0d6f4978dd8d8c2701b',1,'AMReX_FLUXREG_F.H']]],
-  ['_5fslabservice_5fh_5f',['_SLABSERVICE_H_',['../_a_m_re_x___slab_service_8_h.html#ae121be40849f3a0a903f018721a7c16a',1,'AMReX_SlabService.H']]]
+  ['_5febfluxfab_5fh_5f',['_EBFLUXFAB_H_',['../_a_m_re_x___e_b_flux_f_a_b_8_h.html#a76b32f517f0d5db263475754041d4e0d',1,'AMReX_EBFluxFAB.H']]],
+  ['_5ffluxreg_5ff_5fh_5f',['_FLUXREG_F_H_',['../_a_m_re_x___f_l_u_x_r_e_g___f_8_h.html#af7408d6e8c99e0d6f4978dd8d8c2701b',1,'AMReX_FLUXREG_F.H']]]
 ];
