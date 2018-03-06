01babc2f6a8576f73e13cfcbf872a9678c3cfdf2
Travis CI <atmyers@lbl.gov>
2018-03-06 17:12:41 +0000

    Deploy to GitHub Pages: 231dc485621cf47e5074feeda2693c2b93bd59e1

--- doxygen/search/all_0.js
+++ doxygen/search/all_0.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['_5febfluxfab_5fh_5f',['_EBFLUXFAB_H_',['../_a_m_re_x___e_b_flux_f_a_b_8_h.html#a76b32f517f0d5db263475754041d4e0d',1,'AMReX_EBFluxFAB.H']]],
   ['_5ffluxreg_5ff_5fh_5f',['_FLUXREG_F_H_',['../_a_m_re_x___f_l_u_x_r_e_g___f_8_h.html#af7408d6e8c99e0d6f4978dd8d8c2701b',1,'AMReX_FLUXREG_F.H']]],
   ['_5fkdnode',['_kdnode',['../structamrex_1_1__kdnode.html',1,'amrex']]],
   ['_5fkdres',['_kdres',['../structamrex_1_1__kdres.html',1,'amrex']]],
--- doxygen/search/defines_0.js
+++ doxygen/search/defines_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['_5febfluxfab_5fh_5f',['_EBFLUXFAB_H_',['../_a_m_re_x___e_b_flux_f_a_b_8_h.html#a76b32f517f0d5db263475754041d4e0d',1,'AMReX_EBFluxFAB.H']]],
   ['_5ffluxreg_5ff_5fh_5f',['_FLUXREG_F_H_',['../_a_m_re_x___f_l_u_x_r_e_g___f_8_h.html#af7408d6e8c99e0d6f4978dd8d8c2701b',1,'AMReX_FLUXREG_F.H']]]
 ];
