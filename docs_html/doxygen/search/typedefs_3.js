5a1055612edb44cf1b135a4f6a16729ff5bb945a
Travis CI <atmyers@lbl.gov>
2018-04-19 18:26:23 +0000

    Deploy to GitHub Pages: 9377350b053d062498b3d172eb57110f1ddf710f

--- docs_html/doxygen/search/typedefs_3.js
+++ docs_html/doxygen/search/typedefs_3.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['derivativeproduct',['DerivativeProduct',['../class_normal_derivative.html#ab6431a1ca6a4d989ee6d189966da2b4b',1,'NormalDerivative::DerivativeProduct()'],['../class_normal_derivative_3_01_b_l___s_p_a_c_e_d_i_m_01_4.html#aa1d59c9b88626a1650732280a86596c3',1,'NormalDerivative&lt; BL_SPACEDIM &gt;::DerivativeProduct()']]],
   ['deriveboxmap',['DeriveBoxMap',['../classamrex_1_1_derive_rec.html#a663a12bb95b8a469e521364e65bdcbac',1,'amrex::DeriveRec']]],
-  ['derivefunc',['DeriveFunc',['../_a_m_re_x___derive_8_h.html#a18b65f392bd8bf4d87645d95fca62c29',1,'AMReX_Derive.H']]],
-  ['derivefunc3d',['DeriveFunc3D',['../_a_m_re_x___derive_8_h.html#af5f9eff5b9ea3694fdd90fd865bd021c',1,'AMReX_Derive.H']]]
+  ['derivefunc',['DeriveFunc',['../namespaceamrex.html#acb100c3971f39188ccaa96c07517ee1b',1,'amrex']]],
+  ['derivefunc3d',['DeriveFunc3D',['../namespaceamrex.html#a368140c12d35a8d5d1a7ab35b85978fe',1,'amrex']]]
 ];
