fd6e1f2caae62d33251c88e4725201e07e0189bc
Travis CI <atmyers@lbl.gov>
2018-04-20 21:59:40 +0000

    Deploy to GitHub Pages: a86ea2314f5f07a926424cf013be488bca91b646

--- docs_html/doxygen/search/typedefs_1.js
+++ docs_html/doxygen/search/typedefs_1.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['bdcutcellmoments',['BdCutCellMoments',['../class_cut_cell_moments.html#a42c0e65bfafe49e2e0089f28009b2d57',1,'CutCellMoments::BdCutCellMoments()'],['../class_minimal_c_c_c_m.html#a100e6e6008b2f40d617c4fa632c441a2',1,'MinimalCCCM::BdCutCellMoments()']]],
-  ['bdinterpfunc',['BDInterpFunc',['../namespaceamrex.html#a3e15bfd769b25561b5c20c6c26cb80b4',1,'amrex']]],
   ['bndryfunc3ddefault',['BndryFunc3DDefault',['../namespaceamrex.html#ac7b3adaf83de740cb8bfebcaa309fd6b',1,'amrex']]],
   ['bndryfuncdefault',['BndryFuncDefault',['../namespaceamrex.html#af4644e325d68829f48fa9f707a27d38d',1,'amrex']]],
   ['buildnode',['BUILDNODE',['../namespaceamrex.html#af19dcd3222891ed9e496667964e161de',1,'amrex']]]
