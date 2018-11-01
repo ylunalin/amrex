f952f190574762850c1e10f8cec176d6a6f95baa
Travis CI <atmyers@lbl.gov>
2018-11-01 17:10:03 +0000

    Deploy to GitHub Pages: 716d57dec1adad263055fdb1219481faec948ebe

--- docs_html/doxygen/search/functions_18.js
+++ docs_html/doxygen/search/functions_18.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['xpay',['Xpay',['../classamrex_1_1_multi_fab.html#aa06c264011c21bb5a97070ae7cf250d3',1,'amrex::MultiFab::Xpay()'],['../classamrex_1_1_base_fab.html#a022c8c875be15d281d891c00640407bb',1,'amrex::BaseFab::xpay()']]]
+  ['xpay',['xpay',['../classamrex_1_1_base_fab.html#a022c8c875be15d281d891c00640407bb',1,'amrex::BaseFab::xpay()'],['../classamrex_1_1_multi_fab.html#aa06c264011c21bb5a97070ae7cf250d3',1,'amrex::MultiFab::Xpay()']]]
 ];
