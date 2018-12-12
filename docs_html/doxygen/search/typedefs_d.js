bacd1d9f0d0af5742c6aa93848098b6ae3c62aab
Travis CI <atmyers@lbl.gov>
2018-12-12 16:20:09 +0000

    Deploy to GitHub Pages: 15cae6e5342862eafec9997e1355db9d34845374

--- docs_html/doxygen/search/typedefs_d.js
+++ docs_html/doxygen/search/typedefs_d.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['real',['Real',['../structamrex_1_1_particle_tile.html#a23a29b81ce71d9e9249906060113cc21',1,'amrex::ParticleTile']]],
   ['realarray',['RealArray',['../namespaceamrex.html#a9de48c04056551924746c42f9eda3c8f',1,'amrex']]],
   ['realtuple',['RealTuple',['../classamrex_1_1_bndry_data.html#a7d818a2a4471876ea96580404b662b0f',1,'amrex::BndryData']]],
   ['realtype',['RealType',['../classamrex_1_1_array_of_structs.html#a6694efa287bd1d39554c85d773d1a894',1,'amrex::ArrayOfStructs::RealType()'],['../structamrex_1_1_particle.html#a57e2e923cf77341e715f8cf41b15fcac',1,'amrex::Particle::RealType()'],['../structamrex_1_1_particle_comm_data.html#a8cd595a307d981117b1ca716232e621c',1,'amrex::ParticleCommData::RealType()'],['../classamrex_1_1_particle_container.html#aa415f73a03aab24593ffe0d99075e329',1,'amrex::ParticleContainer::RealType()']]],
