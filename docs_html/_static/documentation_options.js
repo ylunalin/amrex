43852dc49c43bd3dd4227776d5311db397754e28
Travis CI <atmyers@lbl.gov>
2019-01-01 13:12:00 +0000

    Deploy to GitHub Pages: e991da7e44177626617d8048aa0bc9c800ed45ea

--- docs_html/_static/documentation_options.js
+++ docs_html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '18.01-dev',
+    VERSION: '19.02-dev',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
