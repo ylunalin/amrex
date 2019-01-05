6f77e7104b78d2bad23b7d743381dd630b8ee427
Travis CI <atmyers@lbl.gov>
2019-01-05 15:09:48 +0000

    Deploy to GitHub Pages: 65047451c1652ca2f0238ecd0fa53eec4221432c

--- docs_html/doxygen/search/functions_15.js
+++ docs_html/doxygen/search/functions_15.js
@@ -2,8 +2,7 @@ var searchData=
 [
   ['ubound',['ubound',['../namespaceamrex.html#a76bb9f31eb00271b10b7c2bf4532ae8c',1,'amrex']]],
   ['ungrownbox',['UngrownBox',['../classamrex_1_1_fill_patch_iterator.html#a8b28e83d795edcadebe049e773b4a136',1,'amrex::FillPatchIterator']]],
-  ['union_5febf',['union_ebf',['../classamrex_1_1_l_s_factory.html#a8112b1f03db1ce9c1db4f4af70c1b800',1,'amrex::LSFactory']]],
-  ['union_5fimpfunc',['union_impfunc',['../classamrex_1_1_l_s_factory.html#a4000a44d80e00c5342e8822c299b84b6',1,'amrex::LSFactory']]],
+  ['union',['Union',['../classamrex_1_1_l_s_factory.html#a7d434880721e515d6458e198ba579219',1,'amrex::LSFactory::Union(const EBFArrayBoxFactory &amp;eb_factory, const MultiFab &amp;mf_impfunc)'],['../classamrex_1_1_l_s_factory.html#a350082120b504041512983b0960bcb8b',1,'amrex::LSFactory::Union(const EBFArrayBoxFactory &amp;eb_factory, const MultiFab &amp;mf_impfunc, int ebt_size)'],['../classamrex_1_1_l_s_factory.html#a37f562a76e4efdb4a4eab2495910fd1f',1,'amrex::LSFactory::Union(const EBFArrayBoxFactory &amp;eb_factory, const MultiFab &amp;mf_impfunc, const IntVect &amp;ebt_size)'],['../classamrex_1_1_l_s_factory.html#aacb082130197d26c131f91f9c12e281d',1,'amrex::LSFactory::Union(const MultiFab &amp;mf_impfunc, bool apply_threshold=false)']]],
   ['uniqify',['uniqify',['../classamrex_1_1_box_array.html#ae3fba31af025b800aef97da09bc4113c',1,'amrex::BoxArray']]],
   ['uniquerandomsubset',['UniqueRandomSubset',['../namespaceamrex.html#aa2c1209819cd8dd79499c2b9f088120e',1,'amrex']]],
   ['uniquestring',['UniqueString',['../namespaceamrex.html#aebee05e08302fb03e29f4270e933239c',1,'amrex']]],
